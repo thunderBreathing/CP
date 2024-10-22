6a11cacec7a68c6125f3b4c40cdf023446598c31
Mahmud Abdul Aziz Rohan <cp.abdulazizrohan@gmail.com>
2024-10-22 18:49:59 +0600

    I don't know

---
+++ USACO/Superprime Rib.cpp
@@ -0,0 +1,42 @@
+/*
+ID: mehranh1
+TASK:
+LANG: C++
+*/
+
+#include <bits/stdc++.h>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> ii;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef map<int, int> mii;
+
+#define el "\n"
+#define pb push_back
+#define mp make_pair
+
+const int INF = 1e9;
+const ll LINF = 4e18;
+const double EPS = 1e-9;
+
+bool debug = false;
+
+void solve() {}
+
+int main() {
+  // ios_base::sync_with_stdio(false);
+  // cin.tie(NULL);
+
+  ifstream fin(".in");
+  ofstream fout(".out");
+
+  fin.close();
+  fout.close();
+
+  return 0;
+}
