ddf0060cb83d3bf69d2f66d3766c941bb94e0d84
thunderBreathing <cp.abdulazizrohan@gmail.com>
2024-10-23 22:23:47 +0600

    solved USACO Superprime Rib

--- USACO/Superprime Rib.cpp
+++ USACO/Superprime Rib.cpp
@@ -1,10 +1,11 @@
 /*
 ID: mehranh1
-TASK:
+TASK: sprime
 LANG: C++
 */
 
 #include <bits/stdc++.h>
+#include <cmath>
 #include <fstream>
 #include <iostream>
 
@@ -26,16 +27,78 @@ const double EPS = 1e-9;
 
 bool debug = false;
 
-void solve() {}
+int n;
+
+ll _sieve_size;
+bitset<10000010> bs;
+vi p;
+
+void sieve(int upperbound) {
+  _sieve_size = upperbound + 1;
+  bs.set();
+  bs[0] = bs[1] = 0;
+  for (ll i = 2; i < _sieve_size; i++)
+    if (bs[i]) {
+      for (ll j = i * i; j <= _sieve_size; j += i) {
+        bs[j] = 0;
+      }
+      p.pb((int)i);
+    }
+}
+
+bool isPrime(int N) {
+  if (N < _sieve_size) {
+    return bs[N];
+  }
+  for (int i = 0; i < (int)p.size() && (ll)p[i] * (ll)p[i] <= (ll)N; i++) {
+    if (N % p[i] == 0)
+      return false;
+  }
+  return true;
+}
+
+vi ans;
+
+void gen_num(int num) {
+  if (num >= pow(10, n - 1) && num < pow(10, n)) {
+    ans.pb(num);
+  }
+
+  for (int i = 1; i < 10; i += 2) {
+    num = num * 10 + i;
+    if (isPrime(num)) {
+      gen_num(num);
+    }
+    num = num / 10;
+  }
+}
+
+void solve() {
+  gen_num(2);
+  gen_num(3);
+  gen_num(5);
+  gen_num(7);
+}
 
 int main() {
   // ios_base::sync_with_stdio(false);
   // cin.tie(NULL);
 
-  ifstream fin(".in");
-  ofstream fout(".out");
+  sieve(10000000);
 
+  ifstream fin("sprime.in");
+
+  fin >> n;
+  // cin >> n;
   fin.close();
+
+  solve();
+
+  ofstream fout("sprime.out");
+  for (int var : ans) {
+    fout << var << el;
+    // cout << var << el;
+  }
   fout.close();
 
   return 0;
