0ccd1801ec1e88447b44aa7e972373c663b5946c
thunderBreathing <cp.abdulazizrohan@gmail.com>
2024-11-01 10:46:45 +0600

    made a script with the help of ChatGPT to automate code running

---
+++ run_cpp.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Usage: $0 <filename.cpp>"
+  exit 1
+fi
+
+filename="$1"
+output="${filename%.*}"
+
+# Compile with warnings and optimization
+g++ -std=c++17 -Wall -Wextra -o "$output" "$filename"
+
+if [ $? -eq 0 ]; then
+  # Measure time and memory usage
+  /usr/bin/time -v "./$output"
+else
+  echo "Compilation failed!"
+fi
+
